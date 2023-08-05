0020f9250849d7177d8fb43fefc93b21b8ef19c3
IlhamStoree <140697315+IlhamStoree@users.noreply.github.com>
2023-08-05 10:55:04 +0700

    Update menu3.sh

--- theme/menu3.sh
+++ theme/menu3.sh
@@ -1,7 +1,7 @@
  #!/bin/bash
 
 BURIQ () {
-    curl -sS https://raw.githubusercontent.com/harimu63/ipkita/main/izin > /root/tmp
+    curl -sS https://raw.githubusercontent.com/IlhamStoree/permission/main/ipvps > /root/tmp
     data=( `cat /root/tmp | grep -E "^### " | awk '{print $2}'` )
     for user in "${data[@]}"
     do
@@ -19,7 +19,7 @@ BURIQ () {
 }
 
 MYIP=$(curl -sS ipv4.icanhazip.com)
-Name=$(curl -sS https://raw.githubusercontent.com/harimu63/ipkita/main/izin | grep $MYIP | awk '{print $2}')
+Name=$(curl -sS https://raw.githubusercontent.com/IlhamStoree/permission/main/ipvps | grep $MYIP | awk '{print $2}')
 echo $Name > /usr/local/etc/.$Name.ini
 CekOne=$(cat /usr/local/etc/.$Name.ini)
 
@@ -36,7 +36,7 @@ fi
 
 PERMISSION () {
     MYIP=$(curl -sS ipv4.icanhazip.com)
-    IZIN=$(curl -sS https://raw.githubusercontent.com/harimu63/ipkita/main/izin | awk '{print $4}' | grep $MYIP)
+    IZIN=$(curl -sS https://raw.githubusercontent.com/IlhamStoree/permission/main/ipvps | awk '{print $4}' | grep $MYIP)
     if [ "$MYIP" = "$IZIN" ]; then
     Bloman
     else
@@ -54,7 +54,7 @@ PERMISSION
 if [ "$res" = "Expired" ]; then
 Exp="\e[36mExpired\033[0m"
 else
-Exp=$(curl -sS https://raw.githubusercontent.com/harimu63/ipkita/main/izin | grep $MYIP | awk '{print $3}')
+Exp=$(curl -sS https://raw.githubusercontent.com/IlhamStoree/permission/main/ipvps | grep $MYIP | awk '{print $3}')
 fi
 
 # =========================================
