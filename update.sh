f80d3bfbe62f012d5d64af75524cdeb8a175df7a
IlhamStoree <140697315+IlhamStoree@users.noreply.github.com>
2023-10-05 19:14:01 +0700

    Update update.sh

--- update.sh
+++ update.sh
@@ -25,7 +25,7 @@ box=$(cat /etc/box)
 # CEK UPDATE
 Green_font_prefix="\033[32m" && Red_font_prefix="\033[31m" && Green_background_prefix="\033[42;37m" && Red_background_prefix="\033[41;37m" && Font_color_suffix="\033[0m"
 Info1="${Green_font_prefix}($version)${Font_color_suffix}"
-Info2="${Green_font_prefix}(LATEST VERSION)${Font_color_suffix}"
+Info2="${Green_font_prefix}(OLD VERSION)${Font_color_suffix}"
 Error="Version ${Green_font_prefix}[$ver]${Font_color_suffix} available${Red_font_prefix}[Please Update]${Font_color_suffix}"
 version=$(cat /home/ver)
 new_version=$( curl https://raw.githubusercontent.com/IlhamStoree/v6/main/newversion | grep $version )
