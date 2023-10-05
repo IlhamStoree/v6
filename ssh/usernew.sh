eb2146ddf6188e8019774938f612490acc138abe
IlhamStoree <140697315+IlhamStoree@users.noreply.github.com>
2023-10-05 19:38:11 +0700

    Update usernew.sh

--- ssh/usernew.sh
+++ ssh/usernew.sh
@@ -4,8 +4,8 @@ biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
 #########################
 # Getting
 clear
-export CHATID="5736569839"
-export KEY="6307367714:AAG_Gae1CEjB-BDTYLzHSb89kbmLxzln7mk"
+export CHATID="1813590936"
+export KEY="6159615642:AAEoAjxKYMwN81Pcgas7b-F2GFYRB0kQomU"
 export TIME="10"
 export URL="https://api.telegram.org/bot$KEY/sendMessage"
 clear
@@ -30,6 +30,7 @@ echo -e "\E[40;1;37m            SSH Account            \E[0m"
 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 read -p "Username : " Login
 read -p "Password : " Pass
+read -p "limit ip : " limit
 read -p "Expired (hari): " masaaktif
 
 IP=$(curl -sS ifconfig.me);
@@ -56,37 +57,36 @@ if [[ ! -z "${PID}" ]]; then
 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-user.log
 echo -e "\E[40;1;37m            SSH Account            \E[0m" | tee -a /etc/log-create-user.log
 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-user.log
-echo -e "💥Username    : $Login" | tee -a /etc/log-create-user.log
-echo -e "💥Password    : $Pass" | tee -a /etc/log-create-user.log
-echo -e "💥Expired On  : $exp" | tee -a /etc/log-create-user.log
+echo -e "Username    : $Login" | tee -a /etc/log-create-user.log
+echo -e "Password    : $Pass" | tee -a /etc/log-create-user.log
+echo -e "Expired On  : $exp" | tee -a /etc/log-create-user.log
 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-user.log
-echo -e "🔰IP          : $IP" | tee -a /etc/log-create-user.log
-echo -e "🔰Host        : $domen" | tee -a /etc/log-create-user.log
-echo -e "🔰NS DOMAIN   : $sldomain" | tee -a /etc/log-create-user.log
-echo -e "🔰SERVER PUB  : $slkey" | tee -a /etc/log-create-user.log
-echo -e "🔰OpenSSH     : $opensh" | tee -a /etc/log-create-user.log
-echo -e "🔰SSH-WS      : $portsshws" | tee -a /etc/log-create-user.log
-echo -e "🔰SSH-SSL-WS  : $wsssl" | tee -a /etc/log-create-user.log
-echo -e "🔰SSL/TLS     : $ssl" | tee -a /etc/log-create-user.log
-echo -e "🔰PORT SLWDNS : 443,80,53" | tee -a /etc/log-create-user.log
-echo -e "🔰UDPGW       : 7100-7300" | tee -a /etc/log-create-user.log
-echo -e "🔰SSH-UDP     : 1-65535" | tee -a /etc/log-create-user.log
-echo -e "🔰SSH-80      : $domen:80@$Login:$Pass" | tee -a /etc/log-create-user.log
-echo -e "🔰SSH-443     : $domen:443@$Login:$Pass" | tee -a /etc/log-create-user.log
-echo -e "🔰SSHUDP      : $domen:1-65535@$Login:$Pass" | tee -a /etc/log-create-user.log
+echo -e "IP          : $IP" | tee -a /etc/log-create-user.log
+echo -e "Host        : $domen" | tee -a /etc/log-create-user.log
+echo -e "OpenSSH     : $opensh" | tee -a /etc/log-create-user.log
+echo -e "SSH-WS      : $portsshws" | tee -a /etc/log-create-user.log
+echo -e "SSH-SSL-WS  : $wsssl" | tee -a /etc/log-create-user.log
+echo -e "SSL/TLS     : $ssl" | tee -a /etc/log-create-user.log
+echo -e "UDPGW       : 7100-7300" | tee -a /etc/log-create-user.log
+echo -e "SSH-UDP     : 1-65535" | tee -a /etc/log-create-user.log
 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-user.log
-echo -e "📅Expired On  : $exp" | tee -a /etc/log-create-user.log
+echo -e "SSHUDP      : $domen:1-65535@$Login:$Pass" | tee -a /etc/log-create-user.log
 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-user.log
-echo -e "🔰Payload WSS" | tee -a /etc/log-create-user.log
+echo -e "PORT SLWDNS : 443,80,53" | tee -a /etc/log-create-user.log
+echo -e "NS DOMAIN   : $sldomain" | tee -a /etc/log-create-user.log
+echo -e "SERVER PUB  : $slkey" | tee -a /etc/log-create-user.log
+echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-user.log
+echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-user.log
+echo -e "Expired On  : $exp" | tee -a /etc/log-create-user.log
+echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-user.log
+echo -e "Payload WSS" | tee -a /etc/log-create-user.log
 echo -e "
 GET wss://isi_bug_disini [protocol][crlf]Host: ${domen}[crlf]Upgrade: websocket[crlf][crlf]
 " | tee -a /etc/log-create-user.log
-echo -e "🔰Payload WS" | tee -a /etc/log-create-user.log
+echo -e "Payload WS" | tee -a /etc/log-create-user.log
 echo -e "
 GET / HTTP/1.1[crlf]Host: $domen[crlf]Upgrade: websocket[crlf][crlf]
 " | tee -a /etc/log-create-user.log
-echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-user.log
-echo -e "\E[40;1;37m  SCRIPT PREMIUM BY ILHAM STORE   \E[0m" | tee -a /etc/log-create-user.log
 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-user.log
 
 else
@@ -94,39 +94,39 @@ else
 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-user.log
 echo -e "\E[40;1;37m            SSH Account            \E[0m" | tee -a /etc/log-create-user.log
 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-user.log
-echo -e "💥Username    : $Login" | tee -a /etc/log-create-user.log
-echo -e "💥Password    : $Pass" | tee -a /etc/log-create-user.log
-echo -e "💥Expired On  : $exp" | tee -a /etc/log-create-user.log
+echo -e "🔥Username    : $Login" | tee -a /etc/log-create-user.log
+echo -e "🔥Password    : $Pass" | tee -a /etc/log-create-user.log
+echo -e "🔥limit ip    : $limit" | tee -a /etc/log-create-user.log
+echo -e "Expired On  : $exp" | tee -a /etc/log-create-user.log
 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-user.log
 echo -e "🔰IP          : $IP" | tee -a /etc/log-create-user.log
 echo -e "🔰Host        : $domen" | tee -a /etc/log-create-user.log
-echo -e "🔰NS DOMAIN   : $sldomain" | tee -a /etc/log-create-user.log
-echo -e "🔰SERVER PUB  : $slkey" | tee -a /etc/log-create-user.log
 echo -e "🔰OpenSSH     : 22" | tee -a /etc/log-create-user.log
 echo -e "🔰SSH-WS      : 80" | tee -a /etc/log-create-user.log
 echo -e "🔰SSH-SSL-WS  : 443" | tee -a /etc/log-create-user.log
 echo -e "🔰SSL/TLS     : 447 , 777" | tee -a /etc/log-create-user.log
 echo -e "🔰UDPGW       : 7100-7300" | tee -a /etc/log-create-user.log
-echo -e "🔰PORT SLWDNS : 443,80,53" | tee -a /etc/log-create-user.log
 echo -e "🔰SSH-UDP     : 1-65535" | tee -a /etc/log-create-user.log
-echo -e "🔰SSH-80      : $domen:80@$Login:$Pass" | tee -a /etc/log-create-user.log
-echo -e "🔰SSH-443     : $domen:443@$Login:$Pass" | tee -a /etc/log-create-user.log
+echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-user.log
 echo -e "🔰SSH UDP     : $domen:1-65535@$Login:$Pass" | tee -a /etc/log-create-user.log
 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-user.log
-echo -e "📅Expired On  : $exp" | tee -a /etc/log-create-user.log
+echo -e "🔰PORT SLWDNS : 443,80,53" | tee -a /etc/log-create-user.log
+echo -e "🔰NS DOMAIN   : $sldomain" | tee -a /etc/log-create-user.log
+echo -e "🔰SERVER PUB  : $slkey" | tee -a /etc/log-create-user.log
 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-user.log
 echo -e "🔰Payload WSS" | tee -a /etc/log-create-user.log
 echo -e "
 GET wss://isi_bug_disini [protocol][crlf]Host: ${domen}[crlf]Upgrade: websocket[crlf][crlf]
 " | tee -a /etc/log-create-user.log
+echo -e ""
 echo -e "🔰Payload WS" | tee -a /etc/log-create-user.log
 echo -e "
 GET / HTTP/1.1[crlf]Host: $domen[crlf]Upgrade: websocket[crlf][crlf]
 " | tee -a /etc/log-create-user.log
 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-user.log
-echo -e "\E[40;1;37m  SCRIPT PREMIUM BY ILHAM STORE  \E[0m" | tee -a /etc/log-create-user.log
+echo -e "\E[40;1;37m  SCRIPT PREMIUM BY ILHAM STORE   \E[0m" | tee -a /etc/log-create-user.log
 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-user.log
 fi
 echo "" | tee -a /etc/log-create-user.log
-read -n 1 -s -r -p "Press any key to back on menu"
+read -n 1 -s -r -p "Press [ENTER] BACK TO MENU"
 menu
